949b735bd725d6be6c0a3fe43cbc55a9c9d54340
jlf44 <jlf44@cam.ac.uk>
2019-12-13 16:16:22 +0000

    Moving DB properties to application.resources

--- src/main/java/uk/cam/lib/cdl/deployment/api/config/DatasourceConfig.java
+++ src/main/java/uk/cam/lib/cdl/deployment/api/config/DatasourceConfig.java
@@ -1,6 +1,7 @@
 package uk.cam.lib.cdl.deployment.api.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -11,14 +12,26 @@ import javax.sql.DataSource;
 @Configuration
 public class DatasourceConfig {
 
+    @Value("${deployment.db.driver}")
+    private String driver;
+
+    @Value("${deployment.db.url}")
+    private String url;
+
+    @Value("${deployment.db.username}")
+    private String username;
+
+    @Value("${deployment.db.password}")
+    private String password;
+
     @Bean
     public DataSource dbDataSource() {
 
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName("org.postgresql.Driver");
-        dataSource.setUrl("jdbc:postgresql://divdb01.its.manchester.ac.uk:5432/dldeployment?autoReconnect=true");
-        dataSource.setUsername("dldeployment");
-        dataSource.setPassword("dq8N7XCgqDeNNwSmUoJx");
+        dataSource.setDriverClassName(driver);
+        dataSource.setUrl(url);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
 
         return dataSource;
     }
